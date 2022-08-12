ebde8179abf5becdb66898249a348c45ddb87ec6
Muhammad Saud Khan <muhammadsaud.khan@cgi.com>
2022-08-12 15:55:21 +0200

    change keycloak settings to prod

--- src/services/service.const.js
+++ src/services/service.const.js
@@ -4,14 +4,23 @@ const MOCK_AUTH_URL = "https://mock--server.herokuapp.com";
 const GOOGLE_CHART_API_URL = "https://chart.googleapis.com";
 const DUMMY_SERVICE = "http://localhost:3000";
 
+// development
+// const INIT_OPTIONS = {
+//     url: 'http://localhost:8088/auth',
+//     clientId: 'Cl13-CX-Battery',
+//     realm: 'CX-Central',
+//     onLoad: 'login-required'
+// };
+
+// production
 const INIT_OPTIONS = {
-    url: 'http://localhost:8088/auth',
+    url: 'https://centralidp.demo.catena-x.net/auth/',
     clientId: 'Cl13-CX-Battery',
     realm: 'CX-Central',
     onLoad: 'login-required'
 };
 
-const REDIRECT_URI = "http://localhost:8080/";
+const REDIRECT_URI = "https://materialpass.int.demo.catena-x.net/";
 
 
 export { TWIN_REGISTRY_URL, AAS_PROXY_URL, MOCK_AUTH_URL, GOOGLE_CHART_API_URL, DUMMY_SERVICE, INIT_OPTIONS, REDIRECT_URI };
