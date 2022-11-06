9e82e98f6dd9229fd3c6a777806bb4378df5ed0a
Muhammad Saud Khan <muhammadsaud.khan@cgi.com>
2022-11-06 16:33:30 +0100

    Restore registry APIs integration work from CMP-271, test CORS issue

--- src/services/service.const.js
+++ src/services/service.const.js
@@ -5,14 +5,22 @@ const MOCK_AUTH_URL = "https://mock--server.herokuapp.com";
 const GOOGLE_CHART_API_URL = "https://chart.googleapis.com";
 const DUMMY_SERVICE = "http://localhost:3000";
 const SERVER_URL = "https://materialpass.int.demo.catena-x.net";
-const BASE_URL = "https://semantics.int.demo.catena-x.net";
+const CX_REGISTRY_URL = "https://semantics.int.demo.catena-x.net";
+const IDP_URL = "https://centralidp.demo.catena-x.net/auth/";
+const API_KEY = "password";
 
 let INIT_OPTIONS = {};
 let REDIRECT_URI = "";
+let CLIENT_CREDENTIALS = {
+  grant_type: 'client_credentials',
+  client_id: '',
+  client_secret: '',
+  scope: 'openid profile email'
+};
 // for production
 if (window.location.href.includes(SERVER_URL)) {
   INIT_OPTIONS = {
-    url: 'https://centralidp.demo.catena-x.net/auth/',
+    url: IDP_URL,
     clientId: 'Cl13-CX-Battery',
     realm: 'CX-Central',
     onLoad: 'login-required'
@@ -29,5 +37,5 @@ else {
   REDIRECT_URI = "http://localhost:8080/";
 }
 
-export {TWIN_REGISTRY_URL, AAS_PROXY_URL, MOCK_AUTH_URL, GOOGLE_CHART_API_URL, DUMMY_SERVICE, INIT_OPTIONS, REDIRECT_URI, BASE_URL, SERVER_URL};
+export {TWIN_REGISTRY_URL, AAS_PROXY_URL, MOCK_AUTH_URL, GOOGLE_CHART_API_URL, DUMMY_SERVICE, INIT_OPTIONS, REDIRECT_URI, CX_REGISTRY_URL, SERVER_URL, API_KEY, CLIENT_CREDENTIALS, IDP_URL};
 
