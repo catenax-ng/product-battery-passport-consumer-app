53186798686bfd3ee252e0fb19b13259dee8b0a5
Mathias Brunkow Moser <mathias.brunkowmoser@cgi.com>
2023-01-30 14:20:11 +0100

    Added alert

---
+++ src/components/general/Alert.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-alert
+        closable
+        icon="mdi-vuetify"
+        :title=title
+        :text=description
+        :type=type
+        >
+    </v-alert>
+</template>
+
+<script>
+export default {
+ name: "Alert",
+ props: {
+    title: {
+      type: String,
+      default: "Alert",
+    },
+    description: {
+      type: String,
+      default: "Alert Description",
+    },
+    type: {
+      type: String,
+      default: "info",
+    }
+ }  
+}
+</script>
+
+<style>
+
+</style>
